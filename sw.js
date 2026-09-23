089cf8a8931e96c125a5c39aa6a4bc4d0b1f52a2
ivoncruzh <ivonnecruzh3@gmail.com>
2026-09-23 16:39:51 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('empoderarte-v1').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='empoderarte-design-v3';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
