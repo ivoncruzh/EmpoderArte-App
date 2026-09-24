1a9cb7f268dcf5f8f5b9d4a7fa095e14fc894d7c
ivoncruzh <ivonnecruzh3@gmail.com>
2026-09-24 12:41:09 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",async event=>{
-  await caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))));
-  await self.clients.claim();
-});
-self.addEventListener("fetch",event=>{});
+self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',()=>{});
